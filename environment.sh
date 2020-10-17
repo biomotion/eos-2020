748cd5ecdaf8fd8e9a25cfd3c348273799724746
biomotion <bensonpuppy1997@gmail.com>
2020-10-17 19:45:37 +0800

    pass microtime environment to container

--- environment.sh
+++ environment.sh
@@ -6,3 +6,6 @@ export PATH=$PATH:$REPO/scripts
 
 export EOS_IMAGE_NAME=eos-2020:latest
 export EOS_CONTAINER_NAME=eos
+
+export MICROTIME_HOST_PATH=$HOME/microtime
+export MICROTIME_PATH=/hostroot$MICROTIME_HOST_PATH
