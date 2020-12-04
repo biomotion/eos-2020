ff23720c59eadd392a49a0f5e3d8265ca713335c
biomotion <bensonpuppy1997@gmail.com>
2020-12-04 15:59:03 +0800

    update docker environment

--- docker/arm_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-source $HOME/.arm-env
-exec "$@"
--- environment.sh
+++ environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export EOS_REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export PATH=$PATH:$REPO/scripts
+export PATH=$PATH:$EOS_REPO/scripts
 
 export EOS_IMAGE_NAME=eos-2020:latest
 export EOS_CONTAINER_NAME=eos
