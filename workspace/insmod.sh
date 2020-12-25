a99fe6f7dee08d78f90f3f8adfafedcbf1b6e6dc
biomotion <bensonpuppy1997@gmail.com>
2020-12-25 18:42:56 +0800

    update mount and insmod scripts

--- workspace/insmod.sh
+++ workspace/insmod.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+
+mknod /dev/lcd c 120 0
 insmod /lib/modules/2.6.15.3/kernel/drivers/char/creator-pxa270-lcd.ko
--- workspace/mount_nfs.sh
+++ workspace/mount_nfs.sh
@@ -5,4 +5,4 @@ ifconfig eth0 10.42.0.100 broadcast 10.42.0.255 netmask 255.255.255.0
 route add default gw 10.42.0.1
 
 portmap&
-mount 10.42.0.1:/home/biomotion/eos-2020/workspace /mnt
+mount -o tcp 10.42.0.1:/home/biomotion/eos-2020/workspace /mnt
